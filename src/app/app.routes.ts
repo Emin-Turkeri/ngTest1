a99d19db22db586a56bb161bae2ecc3122cb244f
emin <me.turkeri@gmail.com>
2024-07-15 15:45:16 +0300

    main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { TestComponent } from '../test/test.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dest',
+    component: TestComponent,
+    title: 'Dest page'
+  }
+];
+
+export default routes;
